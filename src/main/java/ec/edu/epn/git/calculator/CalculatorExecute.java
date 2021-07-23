19c7266b3712bc1cd34757866c7515180f9cf5a0
David-Morales-M <74932387+David-Morales-M@users.noreply.github.com>
2021-07-23 17:21:37 -0500

    Creación del archivo JAR

---
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -0,0 +1,18 @@
+package ec.edu.epn.git.calculator;
+
+public class CalculatorExecute {
+
+    public static void main(String[] args){
+        System.out.println("|-- Calculator Execute --|");
+
+        Calculator c = new Calculator();
+
+        int addition = c.addition(5,2);
+        System.out.println("c.addtion(5,2) = " + addition);
+
+        int substraction = c.substraction(9,3);
+        System.out.println("c.substraction(9,3) = " + substraction);
+
+
+    }
+}
